cd09bce3394ee7b0ccd4355c0a8927a9e4e98fa4
Chirag <chirag161198@gmail.com>
2019-06-04 16:10:05 +0530

    Initial Update

---
+++ NewsAPI.py
@@ -0,0 +1,66 @@
+import pickle
+from bs4 import BeautifulSoup
+import requests
+import json
+from sklearn.pipeline import Pipeline
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__,static_folder='./dist')
+
+with open('Modules/LogisticRegression.pickle','rb') as f:
+    logisticRegression = pickle.load(f)
+with open('Modules/vectorizer.pickle','rb') as f:
+    vectorizer = pickle.load(f)
+with open('Modules/encoder.pickle','rb') as f:
+    encoder = pickle.load(f)
+
+pipeline = Pipeline([
+    ('Vectorizer',vectorizer),
+    ('Classifier',logisticRegression)
+])
+
+def NewsJSON(query):
+    url = 'https://www.bbc.co.uk/search?q=' + '+'.join(query.split(' ')) + '&sa_f=search-product&filter=news&suggid='
+    source_code = requests.get(url)
+    plain_text = source_code.text
+    soup = BeautifulSoup(plain_text,features='html.parser')
+    headlines = soup.findAll('h1',itemprop='headline')
+
+    data = []
+    for item in headlines:
+        tag = item.find('a')
+        news = {
+            'title' : tag.text,
+            'category' : encoder.inverse_transform(pipeline.predict([tag.text]))[0],
+            'link' : tag['href']
+        }
+        data.append(news)
+
+    return json.dumps(data)
+
+@app.route('/',defaults={'path': ''})
+@app.route('/<path:path>')
+def home(path):
+    if path!= '':
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder , 'index.html')
+        
+
+
+@app.route('/news/<query>')
+def news(query):
+    print('query =',str(query),sep=' ')
+    return NewsJSON(query)
+    
+@app.route('/news/')
+def emptyquery():
+    print('empty query')
+    return json.dumps([{
+        'status' : 'No response',
+        'error' : 'Empty query'
+    }])
+
+if __name__ == '__main__':
+   app.run()
